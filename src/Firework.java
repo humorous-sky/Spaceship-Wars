bd6ef50ad196501f828cd5a0ff28eb257867a94c
MthreeJohnnyK <s-jzhong@lwsd.org>
2024-04-06 14:56:04 -0700

    Testing :D

--- src/Firework.java
+++ src/Firework.java
@@ -7,6 +7,7 @@ public class Firework extends Ability {
 	}
 	@Override
 	public void activate(Player p) {
+		//shoots in a circle
 		for (double theta = 0.0; theta < 2 * Math.PI; theta += Math.PI/36) {
 			Screen.entitiesToAdd.add(new Ammos((int) p.rect.getCenterX(), (int) p.rect.getCenterY(), 26 * (float) Math.cos(theta), 26 * (float) Math.sin(theta), 5, p.team));
 		}
