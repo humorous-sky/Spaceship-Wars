318519bf05f625a3e1a0041d0d5fdb3c3197a3ab
s-jzhong <s-jzhong@486E294130.lwsd.k12wa.org>
2024-04-24 13:00:07 -0700

    Balance Update

--- src/Tank.java
+++ src/Tank.java
@@ -5,8 +5,8 @@ public class Tank extends Player {
 	public static final int HP = 160;
 	public static final int AMMOS = 80;
 	public static final int FIRERATE = 160;
-	public static final int RELOAD = 5000;
-	public static final int WIDTH = 68;
+	public static final int RELOAD = 6800;
+	public static final int WIDTH = 60;
 	public static final int HEIGHT = 88;
 	public Tank(int x, int y, Ability a) {
 		super(x, y, WIDTH, HEIGHT, HP, DMG, SPEED, FIRERATE, AMMOS, RELOAD, a, Assets.ships[2]);
@@ -14,8 +14,8 @@ public class Tank extends Player {
 	@Override
 	public void fire() {
   	  if (System.currentTimeMillis() >= lastFire + fireRate && fire && ammos > 0 && !reloading) {
-  		  Screen.entitiesToAdd.add(new Ammos((int) rect.getCenterX() + rect.width/4 * (Math.random() > 0.5? 1 : -1) * (Math.random() > 0.5? 1 : 2), (int) rect.y, 0f, 26f, dmg, team));
-  		  Screen.entitiesToAdd.add(new Ammos((int) rect.getCenterX(), (int) rect.getMaxY(), (float) ((Math.random() > 0.5 ? 1 : -1) * (Math.random() * 5f)), -23f, dmg, team));
+  		  Screen.entitiesToAdd.add(new Ammos((int) rect.getCenterX() + rect.width/3 * (Math.random() >= 0.5? 1 : -1), (int) rect.y, 0f, 26f, dmg, team));
+  		  Screen.entitiesToAdd.add(new Ammos((int) rect.getCenterX(), (int) rect.getMaxY(), (float) ((Math.random() >= 0.5 ? 1 : -1) * (Math.random() * 5f)), -23f, dmg, team));
   		  ammos--;
   		  lastFire = System.currentTimeMillis();
   	  }
