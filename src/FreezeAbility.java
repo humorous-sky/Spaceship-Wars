6d4d04ecfe7acf7b671f4f0962f6330d3e8933be
s-zjia <s-zjia@486E294115.lwsd.k12wa.org>
2024-05-22 12:19:48 -0700

    FreezeAbility class finished

--- src/FreezeAbility.java
+++ src/FreezeAbility.java
@@ -1,13 +1,18 @@
 
 public class FreezeAbility extends Ability {
 	private final static int OPTION = 1;
-	private final static int VALUE = 25;
+	private final static int VALUE = 15;
 	public FreezeAbility() {
 		super(OPTION, VALUE);
 	}
 	@Override
 	public void activate(Player p) {
 		//traverse the Screen.entities ArrayList and add a freeze buff lasting for a 
-		//random of 10000-15000 milliseconds to each non-player entity 
+		//random of 5000-10000 milliseconds to each non-player entity 
+		for(Entity a: Screen.entities) {
+			if(! (a instanceof Player)) {
+				a.buffs.add(new Freeze((int)(Math.random()*5001 + 5000)));
+			}
+		}
 	}
 }
