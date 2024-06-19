06080cda17938c3fad271c5e6ffb57855d9ccb16
MthreeJohnnyK <s-jzhong@lwsd.org>
2024-06-19 14:24:27 -0700

    Patch

--- src/Splitter.java
+++ src/Splitter.java
@@ -19,11 +19,27 @@ public class Splitter extends Entity {
 		this.size = size;
 	}
 	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		paintSmall(x, y + rect.height/4f, size + 1, img, g);
+		paintSmall((float) rect.getCenterX(), y + rect.height/4f, size + 1, img, g);
+	}
+	public void paintSmall(float x, float y, int size, BufferedImage img, Graphics g) {
+		if (size > 3) {
+			return;
+		}
+		int multi = (int) Math.pow(2, (size - this.size));
+		drawImage(x, y, rect.width/multi, rect.height/multi, 0f, img, g);
+		paintSmall(x, y + rect.height/(4 * multi), size + 1, img, g);
+		paintSmall(x + rect.width/(2 * multi), y + rect.height/(4 * multi), size + 1, img, g);
+	}
+	@Override
 	public void onOof() {
-		super.onOof();
+		Screen.plr.a.increment(1, size == 1 ? 1 : 0);
+   	  	Screen.entitiesToRemove.add(this);
 		if (size < 3) {
-			Screen.entitiesToAdd.add(new Splitter(rect.x - rect.width, rect.y + rect.height, size + 1, img));
-			Screen.entitiesToAdd.add(new Splitter(rect.x + rect.width, rect.y + rect.height, size + 1, img));
+			Screen.entitiesToAdd.add(new Splitter(rect.x, rect.y, size + 1, img));
+			Screen.entitiesToAdd.add(new Splitter((int) rect.getCenterX(), rect.y, size + 1, img));
 		}
 	}
 }
